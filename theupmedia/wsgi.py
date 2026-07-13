71cb948b743e7f9c6a4f6e513405e04b00bde089
sanjay1994singh <sanjaypython22@gmail.com>
2026-07-13 13:35:51 +0530

    p

--- theupmedia/wsgi.py
+++ theupmedia/wsgi.py
@@ -1,15 +1,9 @@
 import os
-
+from static_ranges import Ranges
+from dj_static import Cling,MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theupmedia.settings")
 
-application = get_wsgi_application()
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
 
-try:
-    from static_ranges import Ranges
-    from dj_static import Cling, MediaCling
-except ImportError:
-    pass
-else:
-    application = Ranges(Cling(MediaCling(application)))
