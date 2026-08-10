768f984f14f0f9267f2e5dfcabec3a019622d244
sanjay1994singh <sanjaypython22@gmail.com>
2026-08-10 09:41:27 +0530

    Use distinct categories in nav context

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,4 @@
 from django.conf import settings
-from django.db.models import Count, Q
-
 from news.models import Category
 
 
@@ -9,8 +7,7 @@ def site_settings(request):
         "SITE_NAME": settings.SITE_NAME,
         "SITE_DOMAIN": settings.SITE_DOMAIN,
         "NAV_CATEGORIES": Category.objects.filter(is_active=True)
-        .order_by()
-        .annotate(published_count=Count("articles", filter=Q(articles__status="published")))
-        .filter(published_count__gt=0)
-        .order_by("-published_count", "name")[:8],
+        .filter(articles__status="published")
+        .distinct()
+        .order_by("name")[:8],
     }
